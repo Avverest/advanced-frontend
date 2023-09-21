7a4b5060a00ea77d26238d2355cd6c4d08c09b71
Nik <aominedaiki3ww@gmail.com>
2023-09-21 20:08:59 +0300

    Add dev server. Add env variables.

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -4,12 +4,14 @@ import path from 'path'
 import { buildPlugins } from './buildPlugins'
 import { buildLoaders } from './buildLoaders'
 import { buildResolvers } from './buildResolvers'
+import { buildDevServer } from './buildDevServer'
 
 export function buildWebpackConfig (options: BuildOptions): webpack.Configuration {
-  const { mode, paths } = options
+  const { mode, paths, isDev } = options
   return {
     mode,
     entry: paths.entry,
+
     output: {
     filename: "[name].[contenthash].js",
       path: paths.build,
@@ -20,5 +22,7 @@ export function buildWebpackConfig (options: BuildOptions): webpack.Configuratio
       rules: buildLoaders(),
     },
     resolve: buildResolvers(),
+    devtool: isDev ? 'inline-source-map' : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined
   }
 }

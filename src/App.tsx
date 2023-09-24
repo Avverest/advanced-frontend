2730b7355f48325ea7c4dac095610f311a47a551
Nik <aominedaiki3ww@gmail.com>
2023-09-24 11:12:29 +0300

    Add context and provider for theme.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import './index.scss'
+import './styles/index.scss'
 import { Link, Route, Routes } from 'react-router-dom'
-import AboutPage from './pages/About/AboutPage'
-import MainPage from './pages/Main/MainPage'
 import { AboutPageAsync } from './pages/About/AboutPage.async'
 import { MainPageAsync } from './pages/Main/MainPage.async'
 import { Suspense } from 'react'
+import { useTheme } from './theme/useTheme'
+
+
 export function App () {
+  const {theme, toggleTheme} = useTheme()
+
   return (
-    <div className='app'>
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to='/about'>About</Link>
       <Link to='/'>Main</Link>
       <Suspense fallback='Loading...'>

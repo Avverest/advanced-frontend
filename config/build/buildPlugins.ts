7fd56ca96e56415b6c209733fc566a982a44a315
Nik <aominedaiki3ww@gmail.com>
2023-09-25 20:58:11 +0300

    Add routing.

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,5 +1,4 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin'
-import path from 'path'
 import webpack from 'webpack'
 import { BuildOptions } from './types/config'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
@@ -10,7 +9,8 @@ export function buildPlugins ({ paths }: BuildOptions): webpack.WebpackPluginIns
       template: paths.html,
     }),
     new MiniCssExtractPlugin({
-      filename: 'css/[name].[contenthash:8].css'
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
     }),
     new webpack.ProgressPlugin()
   ]

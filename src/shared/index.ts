da89d388897d66074ee58e16256a2c27093871b4
Nik <aominedaiki3ww@gmail.com>
2023-09-29 16:56:14 +0300

    Add button component.

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,3 +1,4 @@
 export { classNames } from './lib'
 export { AppRoutes, RoutePath } from './config/routePath/routePath'
 export { AppLink, AppLinkTheme } from './ui/AppLink/AppLink'
+export { Button, ButtonTheme } from './ui/Button/Button'

08f6c8c37af7382676142c2a7b0f767d092533cd
Nik <aominedaiki3ww@gmail.com>
2023-09-29 18:03:50 +0300

    Add sidebar component.

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,23 @@
+import {FC, useState} from 'react'
+import cls from './Sidebar.module.scss'
+import {Button, classNames} from 'shared'
+import {ThemeSwitcher} from 'widgets'
+
+interface SidebarProps {
+}
+export const Sidebar: FC<SidebarProps> = () => {
+  const [open, setOpen] = useState(false)
+
+  const onSidebarToggle = (): void => {
+    setOpen(prevState => !prevState)
+  }
+
+  return (
+    <div className={classNames(cls.Sidebar, { [cls.collapsed]: open })}>
+      <Button variant={open ? 'short' : 'default'} onClick={onSidebarToggle}>Toggle</Button>
+      <div className={cls.switchers}>
+        <ThemeSwitcher />
+      </div>
+    </div>
+  )
+}

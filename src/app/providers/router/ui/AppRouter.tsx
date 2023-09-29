6df65e96512e28d7dfb4cd54d1bfaa35ad20fdc0
Nik <aominedaiki3ww@gmail.com>
2023-09-29 17:00:30 +0300

    Fix spaces.

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -3,11 +3,11 @@ import {Suspense} from "react"
 import {routeConfig} from '../config/routeConfig'
 
 export function AppRouter () {
-    return (
-        <Suspense fallback='Loading...'>
-            <Routes>
-              {Object.values(routeConfig).map(routeParams => <Route {...routeParams} />)}
-            </Routes>
-        </Suspense>
-    );
+  return (
+    <Suspense fallback='Loading...'>
+      <Routes>
+        {Object.values(routeConfig).map((routeParams, index) => <Route {...routeParams} key={index} />)}
+      </Routes>
+    </Suspense>
+  );
 }

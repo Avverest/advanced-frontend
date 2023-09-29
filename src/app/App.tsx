0130abea24330b24b8d679c78b50befe4b72fa9d
Nik <aominedaiki3ww@gmail.com>
2023-09-29 17:01:16 +0300

    Remove button for toggle theme.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,7 +5,7 @@ import {AppRouter} from './providers/router'
 import {Navbar} from 'widgets'
 
 export function App () {
-  const {theme, toggleTheme} = useTheme()
+  const {theme} = useTheme()
 
   return (
     <div className={classNames('app', {}, [theme])}>

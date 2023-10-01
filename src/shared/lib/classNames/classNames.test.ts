25922d4942033e677cd58cb73661aa7df6504e05
Nik <aominedaiki3ww@gmail.com>
2023-10-01 15:15:47 +0300

    Add jest.

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,44 @@
+import { classNames } from './classNames'
+
+describe('classNames', () => {
+  test('without params', () => {
+    expect(classNames('')).toBe('')
+  })
+  test('with single class', () => {
+    expect(classNames('class1')).toBe('class1')
+  })
+  test('with three additional classes', () => {
+    const expected = 'class1 add1 add2 add3'
+    expect(classNames('class1', {}, ['add1', 'add2', 'add3'])).toBe(expected)
+  })
+  test('with mods', () => {
+    const expected = 'class1 add1 add2 add3 mod1 mod2'
+    expect(
+      classNames(
+        'class1',
+        { mod1: true, mod2: true },
+        ['add1', 'add2', 'add3'],
+      ),
+    ).toBe(expected)
+  })
+  test('with one mode true and one false', () => {
+    const expected = 'class1 add1 add2 add3 mod1'
+    expect(
+      classNames(
+        'class1',
+        { mod1: true, mod2: false },
+        ['add1', 'add2', 'add3'],
+      ),
+    ).toBe(expected)
+  })
+  test('with one mode true and one undefined', () => {
+    const expected = 'class1 add1 add2 add3 mod1'
+    expect(
+      classNames(
+        'class1',
+        { mod1: true, mod2: undefined },
+        ['add1', 'add2', 'add3'],
+      ),
+    ).toBe(expected)
+  })
+})

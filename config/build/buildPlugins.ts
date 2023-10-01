758cecc9138ce43f1d80e20985deb09ec6efc214
Nik <aominedaiki3ww@gmail.com>
2023-10-01 11:25:27 +0300

    Add hot reload.

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import HtmlWebpackPlugin from 'html-webpack-plugin'
 import webpack from 'webpack'
 import { BuildOptions } from './types/config'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin'
 
 export function buildPlugins ({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
   return [
@@ -16,5 +17,7 @@ export function buildPlugins ({ paths, isDev }: BuildOptions): webpack.WebpackPl
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev)
     }),
+    new ReactRefreshPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
   ]
 }

c53d3d9b3d0b7fec3c9be5323983cda6d73439cd
Nik <aominedaiki3ww@gmail.com>
2023-10-03 17:00:26 +0300

    Add button for test error boundary. Remove React import from components.

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,22 @@
+import { FC, useEffect, useState } from 'react'
+import { Button } from 'shared'
+
+// TODO: Удалить компонент после окончания тестирования
+export const BugButton: FC = () => {
+  const [error, setError] = useState(false)
+
+  const onThrow = () => setError(true)
+
+  useEffect(
+    () => {
+      if (error) {
+        throw new Error()
+      }
+    },
+    [error],
+  )
+  return (
+    // eslint-disable-next-line i18next/no-literal-string
+    <Button onClick={onThrow}>Call throw!</Button>
+  )
+}
--- src/pages/Main/ui/MainPage.tsx
+++ src/pages/Main/ui/MainPage.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import {useTranslation} from 'react-i18next'
+import { useTranslation } from 'react-i18next'
+import { BugButton } from 'app/providers/ErrorBoundary'
 
 const MainPage = () => {
-  const {t} = useTranslation('main')
+  const { t } = useTranslation('main')
+
   return (
-    <div>{t('Главная')}</div>
+    <div>
+      <BugButton />
+      {t('Главная')}
+    </div>
   )
 }
 

94daaea472d7b84b1c59b949de1efe26b03bc670
Nik <aominedaiki3ww@gmail.com>
2023-10-03 16:58:58 +0300

    Add translations.

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,20 +1,24 @@
-import {FC, useState} from 'react'
+import { FC, useState } from 'react'
+import { Button, classNames } from 'shared'
+import { LanguageSwitcher, ThemeSwitcher } from 'widgets'
+import { useTranslation } from 'react-i18next'
 import cls from './Sidebar.module.scss'
-import {Button, classNames} from 'shared'
-import {LanguageSwitcher, ThemeSwitcher} from 'widgets'
 
 interface SidebarProps {
 }
 export const Sidebar: FC<SidebarProps> = () => {
-  const [open, setOpen] = useState(false)
+  const [collapsed, setCollapsed] = useState(false)
+  const { t } = useTranslation()
 
   const onSidebarToggle = (): void => {
-    setOpen(prevState => !prevState)
+    setCollapsed((prevState) => !prevState)
   }
 
   return (
-    <div className={classNames(cls.Sidebar, { [cls.collapsed]: open })}>
-      <Button variant={open ? 'short' : 'default'} onClick={onSidebarToggle}>Toggle</Button>
+    <div className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed })}>
+      <Button variant={collapsed ? 'short' : 'default'} onClick={onSidebarToggle}>
+        {collapsed ? t('Открыть') : t('Скрыть') }
+      </Button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
         <LanguageSwitcher />

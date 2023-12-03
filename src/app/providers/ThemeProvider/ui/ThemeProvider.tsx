3cb45467c05214a6e10b3ed194fa2ae482feb687
Nik <aominedaiki3ww@gmail.com>
2023-12-03 20:36:16 +0300

    Add modal.

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,16 +1,31 @@
-import { FC, useMemo, useState } from 'react'
+import {
+  FC, useEffect, useMemo, useState,
+} from 'react'
 import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from '../lib/ThemeContext'
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT
 
-export const ThemeProvider: FC = ({ children }) => {
-  const [theme, setTheme] = useState<Theme>(defaultTheme)
+interface ThemeProviderProps {
+  initTheme?: Theme
+}
+export const ThemeProvider: FC<ThemeProviderProps> = ({ children, initTheme }) => {
+  const [theme, setTheme] = useState<Theme>(initTheme ?? defaultTheme)
 
   const defaultProps = useMemo(
     () => ({
       theme,
       setTheme,
     }),
+    [theme],
+  )
+
+  useEffect(
+    () => {
+      const removeTheme = theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT
+      document.body.classList.remove(removeTheme)
+      document.body.classList.add(theme)
+      console.log(theme)
+    },
     [theme],
   )
 
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,5 +1,5 @@
-import { FC } from 'react'
-import { AppLink } from 'shared/ui'
+import { FC, useCallback, useState } from 'react'
+import { AppLink, Button, Modal } from 'shared/ui'
 import { useTranslation } from 'react-i18next'
 import { classNames } from 'shared/lib'
 import cls from './Navbar.module.scss'
@@ -10,13 +10,23 @@ interface NavbarProps {
 
 export const Navbar: FC<NavbarProps> = (props) => {
   const { className } = props
+  const [isAuthModal, setIsAuthModal] = useState(false)
+
+  const onOpen = useCallback(() => setIsAuthModal(true), [])
+
   const { t } = useTranslation()
+
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
       <div className={cls.links}>
         <AppLink to='/about'>{t('О сайте')}</AppLink>
         <AppLink to='/'>{t('Главная')}</AppLink>
+        <Button variant='clear' onClick={onOpen}>{t('Войти')}</Button>
       </div>
+      <Modal isOpen={isAuthModal} onClose={() => setIsAuthModal(false)}>
+        {/* eslint-disable-next-line */}
+        <p>1 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+      </Modal>
     </div>
   )
 }

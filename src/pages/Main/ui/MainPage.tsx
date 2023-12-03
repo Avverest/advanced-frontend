a815071f8bbfd237caad67d337e9f671aef33b94
Nik <aominedaiki3ww@gmail.com>
2023-12-03 20:36:56 +0300

    Fix warnings.

--- src/pages/Main/ui/MainPage.tsx
+++ src/pages/Main/ui/MainPage.tsx
@@ -1,7 +1,5 @@
 import { useTranslation } from 'react-i18next'
 import { BugButton } from 'app/providers/ErrorBoundary'
-import { AppLink, Button, Modal } from 'shared/ui'
-import { useState } from 'react'
 
 const MainPage = () => {
   const { t } = useTranslation('main')

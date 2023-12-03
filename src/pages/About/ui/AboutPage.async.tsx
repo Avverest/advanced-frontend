5eab0667dccd6010dce2bd64aac370b8c21d7eab
Nik <aominedaiki3ww@gmail.com>
2023-12-03 17:44:00 +0300

    Resolve errors.

--- src/pages/About/ui/AboutPage.async.tsx
+++ src/pages/About/ui/AboutPage.async.tsx
@@ -1,6 +1,7 @@
 import { lazy } from 'react'
 
-export const AboutPageAsync = lazy(() => new Promise((resolve) =>
+// TODO Remove this shit with timeout
+export const AboutPageAsync = lazy(() => new Promise((resolve) => {
   // @ts-ignore
-  // TODO Remove this shit with timeout
-  setTimeout(() => resolve(import('./AboutPage')), 1500)))
+  setTimeout(() => resolve(import('./AboutPage')), 1500)
+}))
